6ce44ec4abc68aa5338fdcc00cd3edfbba6d3aa7
redouane <benbrahimredouane664@gmail.com>
2026-02-25 14:38:55 +0000

    feat: add variable fillable and make in it the necessary fields

--- app/Models/Colocation.php
+++ app/Models/Colocation.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Colocation extends Model
 {
     //
+     protected $fillable = [
+        'owner_id',
+        'name', 
+        'status',   
+    ];
 }
