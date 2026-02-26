05fe87a061bf95b1f1ef00e5a11a71aec829b2f0
redouane <benbrahimredouane664@gmail.com>
2026-02-26 20:52:03 +0000

    chore: add reltion between colocation and owner member membrships invitation and category

--- app/Models/Colocation.php
+++ app/Models/Colocation.php
@@ -7,24 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 class Colocation extends Model
 {
     //
-     protected $fillable = [
+    protected $fillable = [
         'owner_id',
-        'name', 
-        'status', 
-        'cancelled_at',  
+        'name',
+        'status',
+        'cancelled_at',
     ];
 
-    public function owner(){
-        return $this->belongsTo(User::class,'owner_id');
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
     }
 
-    public function memberships(){
+    public function memberships()
+    {
         return $this->hasMany(Membership::class);
     }
-    public function members(){
-        return $this->belongsToMany(User::class,'memberships')->withPivot(['role','left_at'])->withTimestamps();
+    public function members()
+    {
+        return $this->belongsToMany(User::class, 'memberships')->withPivot(['role', 'left_at'])->withTimestamps();
     }
-    public function invitations(){
+    public function invitations()
+    {
         return $this->hasMany(Invitation::class);
+    }
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
     }
 }
