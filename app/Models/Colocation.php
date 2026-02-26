a4e034404bd484d140b6053d87470d659bf51018
redouane <benbrahimredouane664@gmail.com>
2026-02-26 11:03:22 +0000

    feat: add coloms to fillable and necessary relations

--- app/Models/Colocation.php
+++ app/Models/Colocation.php
@@ -10,6 +10,21 @@ class Colocation extends Model
      protected $fillable = [
         'owner_id',
         'name', 
-        'status',   
+        'status', 
+        'cancelled_at',  
     ];
+
+    public function owner(){
+        return $this->belongsTo(User::class,'owner_id');
+    }
+
+    public function memberships(){
+        return $this->hasMany(Membership::class);
+    }
+    public function members(){
+        return $this->belongsToMany(User::class,'memberships')->withPivot(['role','left_at'])->withTimestamps();
+    }
+    public function invitations(){
+        return $this->hasMany(Invitation::class);
+    }
 }
