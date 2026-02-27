31fb62280bd5f370aeb8cbdba9e3fc2c62f29ad6
redouane <benbrahimredouane664@gmail.com>
2026-02-27 06:28:01 +0000

    chore: add variable fillable and casts and necessary relations to expense model

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -3,8 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Expense extends Model
 {
-    //
+    protected $fillable = [
+        'colocation_id',
+        'title',
+        'amount',
+        'date',
+        'category_id',
+        'payer_id',
+    ];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'date' => 'date',
+    ];
+
+  
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function payer()
+    {
+        return $this->belongsTo(User::class, 'payer_id');
+    }
 }
