7c5e7597c9f398e309f5061fbeb3bb5849834a64
redouane <benbrahimredouane664@gmail.com>
2026-02-27 06:32:06 +0000

    chore: finish expenses migration

--- database/migrations/2026_02_25_061430_create_expenses_table.php
+++ database/migrations/2026_02_25_061430_create_expenses_table.php
@@ -9,18 +9,27 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+
     public function up(): void
     {
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('colocation_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('payer_id')->constrained('users')->cascadeOnDelete();
+            $table->string('title');
+            $table->decimal('amount', 10, 2);
+            $table->date('date');
             $table->timestamps();
+            $table->index(['colocation_id', 'date']);
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('expenses');
     }
