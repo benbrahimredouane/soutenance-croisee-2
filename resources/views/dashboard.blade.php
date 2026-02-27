f919e1d36030480d6250cc01f2fd07a642036795
redouane <benbrahimredouane664@gmail.com>
2026-02-27 06:14:08 +0000

    chore: add buttons to join a colocation with token and create one and open one if there is any

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,33 +9,39 @@
 
         @if (!$activeMembership)
 
-            <div class="p-4 bg-yellow-100 rounded mb-4">
-                <h3 class="font-bold mb-2">You are not in any colocation yet.</h3>
-                <p class="mb-4">Create one or join using an invitation.</p>
+        <div class="p-4 bg-yellow-100 rounded mb-4">
+            <h3 class="font-bold mb-2">You are not in any colocation yet.</h3>
+            <p class="mb-4">Create one or join using a token.</p>
 
-                <a href="{{ route('colocations.create') }}"
-                   class="bg-black text-white px-4 py-2 rounded mr-2">
-                    Create Colocation
-                </a>
+            <a href="{{ route('colocations.create') }}"
+                class="bg-black text-white px-4 py-2 rounded mr-2">
+                Create Colocation
+            </a>
+            <a href="{{ route('invitations.joinForm') }}"
+                class="bg-gray-700 text-white px-4 py-2 rounded">
+                Join with Token
+            </a>
 
-            </div>
+        </div>
 
         @else
 
-            <div class="p-4 bg-green-100 rounded">
-                <h3 class="font-bold mb-2">
-                    Your Active Colocation:
-                </h3>
+        <div class="p-4 bg-green-100 rounded">
+            <h3 class="font-bold mb-2">
+                Your Active Colocation:
+            </h3>
 
-                <p class="mb-4">
-                    {{ $activeMembership->colocation->name }}
-                </p>
+            <p class="mb-4">
+                {{ $activeMembership->colocation->name }}
+            </p>
 
-                <a href="{{ route('colocations.show', $activeMembership->colocation) }}"
-                   class="bg-black text-white px-4 py-2 rounded">
-                    Open Colocation
-                </a>
-            </div>
+            <a href="{{ route('colocations.show', $activeMembership->colocation) }}"
+                class="bg-black text-white px-4 py-2 rounded">
+                Open Colocation
+            </a>
+
+            
+        </div>
 
         @endif
 
