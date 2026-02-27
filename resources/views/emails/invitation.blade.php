0be15c89342efcf777aef20af493c0609e52b100
redouane <benbrahimredouane664@gmail.com>
2026-02-27 06:09:19 +0000

    feat: add email content view for token

---
+++ resources/views/emails/invitation.blade.php
@@ -0,0 +1,9 @@
+<h2>You’ve been invited to join: {{ $colocationName }}</h2>
+
+<p>Use this token to join:</p>
+
+<p style="font-size:18px; font-weight:bold;">
+    {{ $token }}
+</p>
+
+<p>Login to your account, go to Join page, and paste the token.</p>
