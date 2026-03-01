4e4a5990ece610d30106de92fb2e34858e0408d5
redouane <benbrahimredouane664@gmail.com>
2026-03-01 15:58:28 +0000

    chore: add nessary foreing keys to settlements table

--- database/migrations/2026_02_27_120840_create_settlements_table.php
+++ database/migrations/2026_02_27_120840_create_settlements_table.php
@@ -14,17 +14,11 @@ return new class extends Migration
     Schema::create('settlements', function (Blueprint $table) {
         $table->id();
 
-        $table->foreignId('colocation_id')
-            ->constrained()
-            ->cascadeOnDelete();
+        $table->foreignId('colocation_id')->constrained()->cascadeOnDelete();
 
-        $table->foreignId('from_user_id')
-            ->constrained('users')
-            ->cascadeOnDelete();
+        $table->foreignId('from_user_id')->constrained('users')->cascadeOnDelete();
 
-        $table->foreignId('to_user_id')
-            ->constrained('users')
-            ->cascadeOnDelete();
+        $table->foreignId('to_user_id')->constrained('users')->cascadeOnDelete();
 
         $table->decimal('amount', 10, 2);
 
